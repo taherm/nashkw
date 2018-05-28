667792a300c1dac2fa52ab315c5e42d4f8d4fa98
usama ahmed <uusa35@gmail.com>
2018-05-28 23:07:16 +0300

    doing some changes again for the client

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -22,28 +22,32 @@
 </head>
 
 <body>
-<input type="hidden" name="language" value="{{ app()->getLocale() }}" id="language">
-@include('frontend.modules.product.partials.quick-view')
+@section('header')
+    <input type="hidden" name="language" value="{{ app()->getLocale() }}" id="language">
+    @include('frontend.modules.product.partials.quick-view')
 <!--[if lt IE 8]>
-<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
-    your browser</a> to improve your experience.</p>
-<![endif]-->
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a
+            href="http://browsehappy.com/">upgrade
+        your browser</a> to improve your experience.</p>
+    <![endif]-->
 
-@include('frontend.partials.header')
+    @include('frontend.partials.header')
 
-@include('frontend.partials._notification')
+    @include('frontend.partials._notification')
+@show
 @section('content')
     @yield('body')
 @show
 
 <!--footer start-->
-@include('frontend.partials.footer')
+@section('footer')
+    @include('frontend.partials.footer')
+@show
 <!--footer end-->
 
 <!--script for this page-->
 @section('scripts')
     @include('frontend.partials.scripts')
 @show
-</body
-        >
+</body>
 </html>
--- resources/views/frontend/pages/under_construction.blade.php
+++ resources/views/frontend/pages/under_construction.blade.php
@@ -1,124 +1,183 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>{{ config('app.name') }}</title>
+{{--<!DOCTYPE html>--}}
+{{--<html>--}}
+{{--<head>--}}
+{{--<title>{{ config('app.name') }}</title>--}}
 
+{{--<link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">--}}
+{{--<style>--}}
+{{--html, body {--}}
+{{--height: auto;--}}
+{{--}--}}
+
+{{--body {--}}
+{{--margin: 0;--}}
+{{--padding: 0;--}}
+{{--color: #B0BEC5;--}}
+{{--font-weight: 100;--}}
+{{--font-family: 'Lato', sans-serif;--}}
+{{--display: flex;--}}
+{{--width: 100%;--}}
+{{--height: auto;--}}
+{{--flex-direction: column;--}}
+{{--justify-content: center;--}}
+{{--align-content: center;--}}
+{{--align-items: center;--}}
+{{--flex-wrap: nowrap;--}}
+{{--}--}}
+
+{{--.container {--}}
+{{--flex-direction: row;--}}
+{{--justify-content: center;--}}
+{{--align-items: center;--}}
+{{--width: 100%;--}}
+{{--overflow: hidden;--}}
+
+{{--}--}}
+
+{{--.content {--}}
+{{--display: flex;--}}
+{{--flex-direction: column;--}}
+{{--justify-content: center;--}}
+{{--align-items: center;--}}
+{{--align-self: center;--}}
+{{--text-align: center;--}}
+{{--flex-wrap: nowrap;--}}
+{{--}--}}
+
+{{--.title {--}}
+{{--font-size: 72px;--}}
+{{--margin-top: 13%;--}}
+{{--text-align: center;--}}
+{{--}--}}
+
+{{--.link {--}}
+{{--text-decoration: none;--}}
+{{--font-size: 72px;--}}
+{{--color: darkgoldenrod;--}}
+{{--}--}}
+
+{{--@media only screen and (max-width: 400px) {--}}
+{{--.pdf-frame {--}}
+{{--width: 200px;--}}
+{{--min-height: 500px;--}}
+{{--}--}}
+
+{{--.header {--}}
+{{--position: fixed;--}}
+{{--top: 0;--}}
+{{--width: 100%;--}}
+{{--height: 10%;--}}
+{{--background-color: white;--}}
+{{--margin-bottom: 10px;--}}
+{{--clear: both;--}}
+{{--border-bottom: 1px solid darkgoldenrod;--}}
+{{--}--}}
+
+{{--.img-logo {--}}
+{{--width: 100%;--}}
+{{--height: auto;--}}
+{{--max-height: 220px;--}}
+{{--border-radius: 10px;--}}
+{{--}--}}
+
+{{--.img-brochure {--}}
+{{--width: 46%;--}}
+{{--height: auto;--}}
+{{--}--}}
+
+{{--.img-wrapper {--}}
+{{--display: flex;--}}
+{{--flex-direction: column;--}}
+{{--justify-content: center;--}}
+{{--align-items: center;--}}
+{{--margin-top: 10px;--}}
+{{--flex-wrap: nowrap;--}}
+{{--width: 46%;--}}
+{{--}--}}
+
+{{--}--}}
+
+{{--@media screen and (min-width: 1000px) {--}}
+{{--.pdf-frame {--}}
+{{--width: 600px;--}}
+{{--min-height: 500px;--}}
+{{--}--}}
+
+{{--.header {--}}
+{{--position: fixed;--}}
+{{--top: 0;--}}
+{{--width: 100%;--}}
+{{--height: 25%;--}}
+{{--background-color: white;--}}
+{{--margin-bottom: 10px;--}}
+{{--clear: both;--}}
+{{--border-bottom: 1px solid darkgoldenrod;--}}
+{{--}--}}
+
+{{--.img-logo {--}}
+{{--width: 25%;--}}
+{{--height: auto;--}}
+{{--max-height: 220px;--}}
+{{--border-radius: 10px;--}}
+{{--}--}}
+
+{{--.img-brochure {--}}
+{{--width: 100%;--}}
+{{--height: auto;--}}
+{{--}--}}
+
+{{--.img-wrapper {--}}
+{{--display: flex;--}}
+{{--flex-direction: column;--}}
+{{--justify-content: center;--}}
+{{--align-items: center;--}}
+{{--margin-top: 10px;--}}
+{{--flex-wrap: nowrap;--}}
+{{--width: 100%;--}}
+{{--}--}}
+{{--}--}}
+
+{{--footer {--}}
+{{--position: fixed;--}}
+{{--bottom: 0;--}}
+{{--width: 100%;--}}
+{{--height: 6%;--}}
+{{--padding: 20px;--}}
+{{--border-top: 1px solid whitesmoke;--}}
+{{--background-color: lightgrey;--}}
+{{--text-align: center;--}}
+{{--justify-content: center;--}}
+{{--align-items: center;--}}
+{{--align-self: center;--}}
+{{--}--}}
+
+{{--a {--}}
+{{--color: darkgoldenrod;--}}
+{{--}--}}
+
+{{--.social {--}}
+{{--font-size: 25px;--}}
+{{--text-align: center;--}}
+{{--margin: 20px--}}
+{{--}--}}
+{{--</style>--}}
+{{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"/>--}}
+{{--</head>--}}
+{{--<body>--}}
+
+@extends('frontend.layouts.master')
+
+@section('styles')
+    @parent
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"/>
     <style>
-        html, body {
-            height: auto;
-        }
-
-        body {
-            margin: 0;
-            padding: 0;
-            color: #B0BEC5;
-            font-weight: 100;
-            font-family: 'Lato', sans-serif;
-            display: flex;
-            width: 100%;
-            height: auto;
-            flex-direction: column;
-            justify-content: center;
-            align-content: center;
-            align-items: center;
-            flex-wrap: nowrap;
-        }
-
-        .container {
-            flex-direction: row;
-            justify-content: center;
-            align-items: center;
-
-        }
-
-        .content {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            align-self: center;
-            text-align: center;
-            flex-wrap: nowrap;
-        }
-
-        .title {
-            font-size: 72px;
-            margin-top: 13%;
-        }
-
         .link {
             text-decoration: none;
-            font-size: 72px;
+            font-size: large;
             color: darkgoldenrod;
         }
-
-        @media only screen and (max-width: 400px) {
-            .pdf-frame {
-                width: 200px;
-                min-height: 500px;
-            }
-
-            .header {
-                position: fixed;
-                top: 0;
-                width: 100%;
-                height: 10%;
-                background-color: white;
-                margin-bottom: 10px;
-                clear: both;
-                border-bottom: 1px solid darkgoldenrod;
-            }
-
-            .img-logo {
-                width: 40%;
-                height: auto;
-                max-height: 220px;
-                border-radius: 10px;
-            }
-
-        }
-
-        @media screen and (max-width: 400px) and (min-width: 1000px), (min-width: 1100px) {
-            .pdf-frame {
-                width: 600px;
-                min-height: 500px;
-            }
-
-            .header {
-                position: fixed;
-                top: 0;
-                width: 100%;
-                height: 25%;
-                background-color: white;
-                margin-bottom: 10px;
-                clear: both;
-                border-bottom: 1px solid darkgoldenrod;
-            }
-
-            .img-logo {
-                width: 25%;
-                height: auto;
-                max-height: 220px;
-                border-radius: 10px;
-            }
-        }
-
-        footer {
-            position: fixed;
-            bottom: 0;
-            width: 100%;
-            height: 6%;
-            padding: 20px;
-            border-top: 1px solid whitesmoke;
-            background-color: lightgrey;
-            text-align: center;
-            justify-content: center;
-            align-items: center;
-            align-self: center;
-        }
-
-        a {
+        a, div , h1, h2, span {
             color: darkgoldenrod;
         }
 
@@ -128,33 +187,33 @@
             margin: 20px
         }
 
-        .img-brochure {
+        footer {
+            position: fixed;
+            bottom: 0;
             width: 100%;
-            height: auto;
-        }
-
-        .img-wrapper {
-            display: flex;
-            flex-direction: column;
+            height: 17%;
+            padding: 20px;
+            border-top: 1px solid whitesmoke;
+            background-color: lightgrey;
+            text-align: center;
             justify-content: center;
             align-items: center;
-            margin-top: 10px;
-            flex-wrap: nowrap
+            align-self: center;
         }
     </style>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"/>
-</head>
-<body>
-<div class="container">
-    <div class="content">
-        <div class="header">
-            <a href="{{ asset('images/brochure.pdf') }}">
-                <img src="{{ asset('images/soon.jpeg') }}" alt="{{ env('APP_NAME') }}" class="img-logo"/>
-            </a>
-        </div>
-        <div class="title">
-            <div>
-                <a href="{{ asset('images/brochure/BROCHURE -2.pdf') }}" class="link">View Our Brochure</a>
+@endsection
+@section('header')
+@stop
+@section('content')
+    <div class="container">
+        <div class="content">
+            <div class="header">
+                <a href="{{ asset('images/brochure.pdf') }}">
+                    <img src="{{ asset('images/soon.jpeg') }}" alt="{{ env('APP_NAME') }}" class="img-logo"/>
+                </a>
+            </div>
+            <div class="col-lg-12" style="text-align: center">
+                    <a href="{{ asset('images/brochure/BROCHURE -2.pdf') }}" class="link">View Our Brochure</a>
             </div>
             <div class="img-wrapper">
                 @for($i=1;$i<=14;$i++)
@@ -162,23 +221,22 @@
                          alt="{{ env('APP_NAME') }}">
                 @endfor
             </div>
-
         </div>
     </div>
-</div>
-<footer>
-    <a href="https://www.instagram.com/harayer7/" class="social" target="_blank"><i
-                class="fa fa-fw fa-instagram"></i></a>
-    <a href="https://www.twitter.com/harayer7/" class="social" target="_blank"><i
-                class="fa fa-fw fa-twitter"></i></a>
-    <a href="https://www.youtube.com/watch?v=Rn1i3vRhU7s" class="social" target="_blank"><i
-                class="fa fa-fw fa-youtube"></i></a>
-    <a href="mailto:info@harayer7.com" class="social" target="_blank"><i
-                class="fa fa-fw fa-inbox"></i></a>
-    <hr>
-    <span style="text-decoration: none; font-size: large; color: black; font-weight: bolder; margin : 15px;">info@harayer7.com لمزيد من المعلومات </span>
-    <br>
-    </div>
-    </br>
-</body>
-</html>
+@endsection
+
+@section('footer')
+    <footer>
+        <a href="https://www.instagram.com/harayer7/" class="social" target="_blank"><i
+                    class="fa fa-fw fa-instagram"></i></a>
+        <a href="https://www.twitter.com/harayer7/" class="social" target="_blank"><i
+                    class="fa fa-fw fa-twitter"></i></a>
+        <a href="https://www.youtube.com/watch?v=Rn1i3vRhU7s" class="social" target="_blank"><i
+                    class="fa fa-fw fa-youtube"></i></a>
+        <a href="mailto:info@harayer7.com" class="social" target="_blank"><i
+                    class="fa fa-fw fa-inbox"></i></a>
+        <hr>
+        <div style="margin: auto; height: 50px; text-decoration: none; font-size: large; color: black; font-weight: bolder;">info@harayer7.com لمزيد من المعلومات </div>
+        </div>
+    </footer>
+@stop
