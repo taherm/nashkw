11d5cee31f83f74e29f7ae090873827a990d439a
usama ahmed <uusa35@gmail.com>
2018-06-24 18:51:31 +0300

    update the css

--- resources/views/backend/partials/styles.blade.php
+++ resources/views/backend/partials/styles.blade.php
@@ -1,1 +1,7 @@
 <link href="{{ mix('css/backend.css') }}" rel="stylesheet">
+<style>
+    @import url('https://fonts.googleapis.com/css?family=ABeeZee');
+    body, html, div, a, p, h1, h2, h3, h4, h5, h6, table, row, td, th, tr, thead, tfoot, button, span, .btn, .btn-del, ul, li, .page-content, .portlet, .light, .profile-sidebar-portlet {
+        font-family: 'ABeeZee', sans-serif;
+    }
+</style>
