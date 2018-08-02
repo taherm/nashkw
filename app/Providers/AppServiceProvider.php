7344e3e208d226dd2531f7a89c1c0dfc798e2389
usama ahmed <uusa35@gmail.com>
2018-08-02 08:03:51 +0300

    update the appservice provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,8 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         if ($this->app->environment('local')) {
-            $this->app->register(DuskServiceProvider::class);
-            $this->app->register(GeneratorsServiceProvider::class);
+//            $this->app->register(DuskServiceProvider::class);
+//            $this->app->register(GeneratorsServiceProvider::class);
         }
     }
 }
