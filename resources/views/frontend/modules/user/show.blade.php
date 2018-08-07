f9df14d3e7b80287e7f82d0e23d513fee563ac64
usama ahmed <uusa35@gmail.com>
2018-08-07 18:25:21 +0300

    update the myaccount

--- resources/views/frontend/modules/user/show.blade.php
+++ resources/views/frontend/modules/user/show.blade.php
@@ -19,7 +19,8 @@
                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                             <div class="contact-info">
                                 <h1>{{ trans('general.order_history') }}</h1>
-                                <table class="table table-bordered text-center" style="font-weight: bolder">
+                                <table class="table table-bordered text-center"
+                                       style="font-weight: bolder; font-size: medium; text-align: justify">
                                     <thead>
                                     <tr class="c-head">
                                         <th>{{ trans('general.id') }}</th>
@@ -28,12 +29,7 @@
                                         <th>{{ trans('general.price') }}</th>
                                         <th>{{ trans('general.discount') }}</th>
                                         <th>{{ trans('general.net_price') }}</th>
-                                        <th>{{ trans('general.email') }}</th>
-                                        <th>{{ trans('general.address') }}/country</th>
-                                        <th>{{ trans('general.mobile') }}</th>
-                                        <th>{{ trans('general.reference_id') }}</th>
-                                        <th>{{ trans('general.payment_method') }}</th>
-                                        <th>{{ trans('general.branch') }}</th>
+                                        <th>{{ trans('general.more_information') }}</th>
                                     </tr>
                                     </thead>
                                     <tr>
@@ -44,12 +40,18 @@
                                             <td>{{ $order->price }}</td>
                                             <td>{{ $order->discount }}</td>
                                             <td>{{ $order->net_price }}</td>
-                                            <td>{{ $order->email }}</td>
-                                            <td>{{ $order->address }} / {{ $order->country }}</td>
-                                            <td>{{ $order->mobile }}</td>
-                                            <td>{{ $order->reference_id }}</td>
-                                            <td>{{ $order->payment_method }}</td>
-                                            <td>{{ $order->branch ? $order->branch->name .'-'. $order->branch->address : ''}}</td>
+
+                                            <td>
+                                                {{ trans('general.email') }} : {{ $order->email }} <br>
+                                                {{ trans('general.mobile') }} : {{ $order->mobile }} <br>
+                                                {{ trans('general.phone') }} : {{ $order->phone}} <br>
+                                                {{ trans('general.address') }} : {{ $order->address}} / {{ $order->country }} <br>
+                                                {{ trans('general.reference_id') }} : {{ $order->reference_id}} <br>
+                                                {{ trans('general.payment_method') }} : {{ $order->payment_method }}
+                                                <br>
+                                                {{ trans('general.delivery_branch_location') }}
+                                                : {{ $order->branch ? $order->branch->name .'-'. $order->branch->address : ''}}
+                                            </td>
                                         @endforeach
                                     </tr>
                                 </table>
