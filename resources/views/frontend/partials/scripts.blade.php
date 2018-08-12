bdacd19bb64b0128d9828691420140cecc101f35
uusa35 <uusa35@gmail.com>
2018-08-12 14:32:56 +0200

    adding mobile feature

--- resources/views/frontend/partials/scripts.blade.php
+++ resources/views/frontend/partials/scripts.blade.php
@@ -1,7 +1,7 @@
 <script src="{{ mix('js/app.js') }}"></script>
 <script src="{{ mix('js/frontend.js') }}"></script>
-@desktop
-@if(app()->isLocale('ar'))
-    <script src="{{ mix('js/frontend-custom-ar.js') }}"></script>
-@endif
-@enddesktop
+{{--@desktop--}}
+{{--@if(app()->isLocale('ar'))--}}
+    {{--<script src="{{ mix('js/frontend-custom-ar.js') }}"></script>--}}
+{{--@endif--}}
+{{--@enddesktop--}}
