d99850e64c55b9b72119ed0c46409a1e9affa831
uusa35 <uusa35@gmail.com>
2018-09-24 18:11:27 +0300

    doing more updates to footer

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -11,7 +11,8 @@
                             <div class="single-footer-service footer-2-service" style="min-height: 95px;">
                                 <div class="footer-service-icon">
                                     {{--<img class="box-icon" src="{{asset('meem/frontend/img/icons/ft-img_2.png')}}" alt="">--}}
-                                    <i class="fa fa-truck fa-fw fa-3x" aria-hidden="true" style="color : white; opacity: 0.4"></i>
+                                    <i class="fa fa-truck fa-fw fa-3x" aria-hidden="true"
+                                       style="color : white; opacity: 0.4"></i>
                                 </div>
                                 <div class="footer-service-text">
                                     <h2>{{ trans('general.free_shipping_in_kuwait') }}</h2>
@@ -82,11 +83,13 @@
                                 <div class="middle-text">
                                     <p>{{ trans('general.online_shopping_in_gulf') }}</p>
                                     <ul class="footer-icon">
-                                        <li><a href="https://www.instagram.com/{{ $settings->instagram }}/" target="_blank"><i
+                                        <li><a href="https://www.instagram.com/{{ $settings->instagram }}/"
+                                               target="_blank"><i
                                                         class="fa fa-instagram"></i></a></li>
                                         <li><a href="https://www.twitter.com/{{ $settings->twitter }}/" target="_blank"><i
                                                         class="fa fa-twitter"></i></a></li>
-                                        <li><a href="https://www.snapchat.com/{{ $settings->snapchat }}/" target="_blank"><i
+                                        <li><a href="https://www.snapchat.com/{{ $settings->snapchat }}/"
+                                               target="_blank"><i
                                                         class="fa fa-snapchat"></i></a></li>
                                         <li><a href="{{ $settings->youtube }}" target="_blank"><i
                                                         class="fa fa-youtube"></i></a></li>
@@ -101,33 +104,45 @@
                                 <h3>{{ trans('general.contactus') }}</h3>
                                 <div class="footer-address">
                                     <ul>
-                                        <li><i class="fa fa-map-marker"> </i>{{ trans('general.address') }}
-                                            : {{$settings->address}}</li>
-                                        {{--<li><i class="fa fa-phone"> </i> </li>--}}
-                                        <li><i class="fa fa-envelope-o"> </i> {{ trans('general.email') }}
-                                            : {{$settings->email}}</li>
+                                        @if(!is_null($settings->address))
+                                            <li><i class="fa fa-map-marker"> </i>{{ trans('general.address') }}
+                                                : {{$settings->address}}</li>
+                                        @endif
+                                        @if(!is_null($settings->email))
+                                            <li><i class="fa fa-envelope-o"> </i>
+                                                {{$settings->email}}</li>
+                                        @endif
+                                        @if(!is_null($settings->mobile))
+                                            <li><i class="fa fa-mobile-phone"> </i>
+                                                {{$settings->mobile}}</li>
+                                        @endif
+                                        @if(!is_null($settings->whatsapp))
+                                            <li><i class="fa fa-whatsapp"> </i>
+                                                {{$settings->whatsapp}}</li>
+                                        @endif
+
                                     </ul>
                                 </div>
                             </div>
                             <!-- middle-footer-text end-->
                         </div>
-                        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
-                            <!-- middle-footer-text start-->
-                            <div class="middle-footer-text middle-footer-text2">
-                                <h3>{{ trans('general.information') }}</h3>
-                                <div class="footer-menu">
-                                    <ul>
-                                        @foreach($pages as $page)
-                                            <li>
-                                                <a href="{{ $page->url }}">{{ $page->slug }}</a>
-                                            </li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                            </div>
-                            <!-- middle-footer-text end-->
-                        </div>
-                        <div class="col-lg-2 col-md-2 hidden-sm col-xs-12">
+                        {{--<div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">--}}
+                            {{--<!-- middle-footer-text start-->--}}
+                            {{--<div class="middle-footer-text middle-footer-text2">--}}
+                                {{--<h3>{{ trans('general.information') }}</h3>--}}
+                                {{--<div class="footer-menu">--}}
+                                    {{--<ul>--}}
+                                        {{--@foreach($pages as $page)--}}
+                                            {{--<li>--}}
+                                                {{--<a href="{{ $page->url }}">{{ $page->slug }}</a>--}}
+                                            {{--</li>--}}
+                                        {{--@endforeach--}}
+                                    {{--</ul>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                            {{--<!-- middle-footer-text end-->--}}
+                        {{--</div>--}}
+                        <div class="col-lg-4 col-md-4 hidden-sm col-xs-12">
                             <!-- middle-footer-text start-->
                             <div class="middle-footer-text middle-footer-text2">
                                 <h3>{{ trans('general.pages') }}</h3>
@@ -176,8 +191,9 @@
                 <div class="row">
                     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                         <div class="footer-copyright" style="color : white;">
-                            <address>{{ trans('general.all_rights_reserved') }} &nbsp; {{ trans('general.designed_by') }}  	&nbsp;
-                                <a href="http://ideasowners.net/">IdeasOwners</a>  	&nbsp;
+                            <address>{{ trans('general.all_rights_reserved') }}
+                                &nbsp; {{ trans('general.designed_by') }} &nbsp;
+                                <a href="http://ideasowners.net/">IdeasOwners</a> &nbsp;
                             </address>
                         </div>
                     </div>
