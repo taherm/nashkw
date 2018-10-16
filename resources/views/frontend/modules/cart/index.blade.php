24b7021b9ec07d675be3cf215e27214dbb991fc0
uusa35 <uusa35@gmail.com>
2018-10-16 13:21:30 +0300

    updates

--- resources/views/frontend/modules/cart/index.blade.php
+++ resources/views/frontend/modules/cart/index.blade.php
@@ -166,7 +166,7 @@
                             </form>
                         </div>
                     </div>
-                @elsesho
+                @else
                     <hr class="page-divider small"/>
                     <div class="col-lg-12">
                         <section class="page-section breadcrumbs">
