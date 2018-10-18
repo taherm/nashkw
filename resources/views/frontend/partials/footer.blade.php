a96210391092d487f20192ebbb33fa4084177c13
uusa35 <uusa35@gmail.com>
2018-10-18 13:33:55 +0300

    updates

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -96,7 +96,7 @@
                     <div class="payments">
                         <ul>
                             @if($currency->currency_symbol_en === 'KWD')
-                                <li><img class="img-grey img-sm" src="{{ asset('img/k-net.png') }}" alt="knet"/></li>
+                                <li><img class="img-grey img-xs" src="{{ asset('img/k-net.png') }}" alt="knet"/></li>
                             @endif
                             <li><img class="img-xs" src="{{ asset('img/preview/payments/visa.jpg') }}" alt="{{ $settings->company }}"/>
                             </li>
