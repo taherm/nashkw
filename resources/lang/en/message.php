a9a7706a83e2cad72f89071b365b2e81e238ab4b
root <uusa35@gmail.com>
2018-10-18 09:24:45 +0000

    trans

---
+++ resources/lang/en/message.php
@@ -0,0 +1,8 @@
+<?php
+
+return array (
+  'loading' => 'Loading ...',
+  'aboutus_message' => 'Lamset 7haraer',
+  'favorite_saved' => 'Product is added to your wish list successfully.',
+  'no_items_found' => 'No Products found.',
+);
