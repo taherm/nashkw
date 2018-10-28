28f54597bd05903b6d37d7bfec282a01c190367d
uusa35 <uusa35@gmail.com>
2018-10-28 17:58:28 +0300

    notification updates

--- resources/views/frontend/partials/head.blade.php
+++ resources/views/frontend/partials/head.blade.php
@@ -6,6 +6,7 @@
 <meta name="author" content="{{ trans('general.app_author') }}">
 <meta name="country" content="kuwait">
 <meta name="lang" content="{{ app()->getLocale() }}">
+<meta name="google-site-verification" content="fy3pTvV0z024nR79nukGxw-tnOmJ2F5BnMeayo-g4-c" />
 <!-- favicon -->
 <link rel="shortcut icon" href="{{ asset('images/logo.ico') }}"/>
 <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('images/logo.jpg') }}">
