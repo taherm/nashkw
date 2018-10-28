b243ea32f3f752039bc24a1755e24ba53e727bd3
uusa35 <uusa35@gmail.com>
2018-10-28 18:34:46 +0300

    notification updates

--- resources/views/frontend/partials/head.blade.php
+++ resources/views/frontend/partials/head.blade.php
@@ -4,8 +4,13 @@
 <meta name="description" content="{{ trans('general.meta_description') }}">
 <meta name="keywords" content="{{ trans('general.app_keywords') }}"/>
 <meta name="author" content="{{ trans('general.app_author') }}">
-<meta name="country" content="kuwait">
-<meta name="lang" content="{{ app()->getLocale() }}">
+<meta name="country" content="{{ $settings->country }}">
+<meta name="mobile" content="{{ $settings->mobile }}">
+<meta name="phone" content="{{ $settings->phone }}">
+<meta name="logo" content="{{ asset(env('LARGE').$settings->logo) }}">
+<meta name="email" content="{{ $settings->email }}">
+<meta name="address" content="{{ $settings->address }}">
+<meta name="name" content="{{ $settings->company }}">
 <meta name="google-site-verification" content="fy3pTvV0z024nR79nukGxw-tnOmJ2F5BnMeayo-g4-c" />
 <!-- favicon -->
 <link rel="shortcut icon" href="{{ asset('images/logo.ico') }}"/>
