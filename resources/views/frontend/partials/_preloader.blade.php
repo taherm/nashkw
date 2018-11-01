9a24adcb430032fed2f0eb984461247ac3ab0f89
uusa35 <uusa35@gmail.com>
2018-11-01 08:50:29 +0300

    updates

--- resources/views/frontend/partials/_preloader.blade.php
+++ resources/views/frontend/partials/_preloader.blade.php
@@ -1,7 +1,7 @@
 <!-- PRELOADER -->
 <div id="preloader">
     <div id="preloader-status">
-        <div id="preloader-title" style="margin: 10px;"><img class="img-sm" src="{{ asset(env('THUMBNAIL').$settings->logo) }}" alt="{{ $settings->name }}"></div>
+        <div id="preloader-title" style="margin: 10px;"><img class="img-sm" src="{{ asset(env('THUMBNAIL').$settings->logo) }}" alt="{{ $settings->company_ar . $settings->company_en }}"></div>
         <div class="spinner">
             <div class="rect1"></div>
             <div class="rect2"></div>
