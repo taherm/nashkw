f0647a01d39d6294875512917617d3d014dbc148
uusa35 <uusa35@gmail.com>
2018-11-26 13:37:00 +0300

    updates

--- resources/views/frontend/partials/_categories_on_home_page.blade.php
+++ resources/views/frontend/partials/_categories_on_home_page.blade.php
@@ -14,21 +14,21 @@
                                        background-size: cover; /* Resize the background image to cover the entire container */
                                        "
                                href="{{ route('frontend.product.search', ['category_id' => $parent->id]) }}">
-                                {{--<div class="img-bg"></div>--}}
-                                {{--<div class="caption">--}}
-                                {{--<div class="caption-wrapper div-table">--}}
-                                {{--<div class="caption-inner div-cell">--}}
-                                {{--<h2 class="caption-title"><span>{{ $parent->name }}</span>--}}
-                                {{--</h2>--}}
-                                {{--@if($parent->description)--}}
-                                {{--<h3 class="caption-sub-title">--}}
-                                {{--<span>{{ $parent->description }}</span>--}}
-                                {{--</h3>--}}
-                                {{--@endif--}}
-                                {{--<span class="btn btn-theme btn-theme-sm">{{ trans('general.shop_now') }}</span>--}}
-                                {{--</div>--}}
-                                {{--</div>--}}
-                                {{--</div>--}}
+                                <div class="img-bg"></div>
+                                <div class="caption">
+                                    <div class="caption-wrapper div-table">
+                                        <div class="caption-inner div-cell">
+                                            <h2 class="caption-title"><span>{{ $parent->name }}</span>
+                                            </h2>
+                                            @if($parent->description)
+                                                <h3 class="caption-sub-title">
+                                                    <span>{{ $parent->description }}</span>
+                                                </h3>
+                                            @endif
+                                            <span class="btn btn-theme btn-theme-sm">{{ trans('general.shop_now') }}</span>
+                                        </div>
+                                    </div>
+                                </div>
                             </a>
                         </div>
                     </div>
