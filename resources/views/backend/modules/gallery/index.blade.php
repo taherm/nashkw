46a040dfea19923438cb5d3c5cf2ae96aa88d96e
Taher Fakhruddin Makadam <mukadam.taher@gmail.com>
2019-12-07 12:41:40 +0300

    changes

--- resources/views/backend/modules/gallery/index.blade.php
+++ resources/views/backend/modules/gallery/index.blade.php
@@ -1,9 +1,8 @@
 @extends('backend.layouts.app')
 
-@section('breadcrumbs')
-{{ Breadcrumbs::render('backend.gallery.index') }}
-@endsection
-
+{{--@section('breadcrumbs')--}}
+{{-- Breadcrumbs::render('backend.gallery.index') --}}
+{{--@endsection--}}
 @section('content')
 <div class="portlet-body">
     <table id="dataTable" class="table table-striped table-bordered table-hover" cellspacing="0" width="100%">
