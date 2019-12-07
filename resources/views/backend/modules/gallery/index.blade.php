7dedb47a9fd37b9a21e5db0e5dbfe32756d8bd0b
Taher Fakhruddin Makadam <mukadam.taher@gmail.com>
2019-12-07 11:41:32 +0300

    changes

--- resources/views/backend/modules/gallery/index.blade.php
+++ resources/views/backend/modules/gallery/index.blade.php
@@ -1,14 +1,13 @@
 @extends('backend.layouts.app')
 
-@section('breadcrumbs')
+{{--@section('breadcrumbs')
     {{ Breadcrumbs::render('backend.gallery.index') }}
-@endsection
+@endsection--}}
 
 @section('content')
-    <div class="portlet-body">
-        <table id="dataTable" class="table table-striped table-bordered table-hover" cellspacing="0"
-               width="100%">
-            <thead>
+<div class="portlet-body">
+    <table id="dataTable" class="table table-striped table-bordered table-hover" cellspacing="0" width="100%">
+        <thead>
             <tr>
                 <th>Id</th>
                 <th>name_ar</th>
@@ -20,8 +19,8 @@
                 <th>active</th>
                 <th>Action</th>
             </tr>
-            </thead>
-            <tfoot>
+        </thead>
+        <tfoot>
             <tr>
                 <th>Id</th>
                 <th>name_ar</th>
@@ -33,61 +32,54 @@
                 <th>active</th>
                 <th>Action</th>
             </tr>
-            </tfoot>
-            <tbody>
+        </tfoot>
+        <tbody>
             @foreach($elements as $element)
-                <tr>
-                    <td>{{ $element->id }}</td>
-                    <td>{{ $element->name_ar }}</td>
-                    <td>{{ $element->name_en }}</td>
-                    <td>{{ $element->galleryable_id}}</td>
-                    <td>
-                        <span class="label label-success">{{ class_basename($element->galleryable_type) }}</span>
-                    </td>
-                    <td>
-                        {{ $element->galleryable_type }}
-                    </td>
-                    <td>
-                        <img class="img-sm"
-                             src="{{ asset('storage/uploads/images/thumbnail/'.$element->cover) }}" alt="">
-                    </td>
-                    <td>
-                        <span class="label {{ activeLabel($element->active) }}">{{ activeText($element->active) }}</span>
-                    </td>
-                    <td>
-                        <div class="btn-group pull-right">
-                            <button type="button" class="btn green btn-sm btn-outline dropdown-toggle"
-                                    data-toggle="dropdown"> Actions
-                                <i class="fa fa-angle-down"></i>
-                            </button>
-                            <ul class="dropdown-menu pull-right" role="menu">
-                                <li>
-                                    <a href="{{ route('backend.activate',['model' => 'gallery', 'id' => $element->id]) }}">
-                                        <i class="fa fa-fw fa-user"></i>toggle activation</a>
-                                </li>
-                                <li>
-                                    <a data-toggle="modal" href="#" data-target="#basic"
-                                       data-title="Delete"
-                                       data-content="Are you sure you want to delete {{ $element->name  }}? "
-                                       data-form_id="delete-{{ $element->id }}"
-                                    >
-                                        <i class="fa fa-fw fa-recycle"></i> delete</a>
-                                    <form method="post" id="delete-{{ $element->id }}"
-                                          action="{{ route('backend.gallery.destroy',$element->id) }}">
-                                        @csrf
-                                        <input type="hidden" name="_method" value="delete"/>
-                                        <button type="submit" class="btn btn-del hidden">
-                                            <i class="fa fa-fw fa-times-circle"></i> delete
-                                        </button>
-                                    </form>
-                                </li>
+            <tr>
+                <td>{{ $element->id }}</td>
+                <td>{{ $element->name_ar }}</td>
+                <td>{{ $element->name_en }}</td>
+                <td>{{ $element->galleryable_id}}</td>
+                <td>
+                    <span class="label label-success">{{ class_basename($element->galleryable_type) }}</span>
+                </td>
+                <td>
+                    {{ $element->galleryable_type }}
+                </td>
+                <td>
+                    <img class="img-sm" src="{{ asset('storage/uploads/images/thumbnail/'.$element->cover) }}" alt="">
+                </td>
+                <td>
+                    <span class="label {{ activeLabel($element->active) }}">{{ activeText($element->active) }}</span>
+                </td>
+                <td>
+                    <div class="btn-group pull-right">
+                        <button type="button" class="btn green btn-sm btn-outline dropdown-toggle" data-toggle="dropdown"> Actions
+                            <i class="fa fa-angle-down"></i>
+                        </button>
+                        <ul class="dropdown-menu pull-right" role="menu">
+                            <li>
+                                <a href="{{ route('backend.activate',['model' => 'gallery', 'id' => $element->id]) }}">
+                                    <i class="fa fa-fw fa-user"></i>toggle activation</a>
+                            </li>
+                            <li>
+                                <a data-toggle="modal" href="#" data-target="#basic" data-title="Delete" data-content="Are you sure you want to delete {{ $element->name  }}? " data-form_id="delete-{{ $element->id }}">
+                                    <i class="fa fa-fw fa-recycle"></i> delete</a>
+                                <form method="post" id="delete-{{ $element->id }}" action="{{ route('backend.gallery.destroy',$element->id) }}">
+                                    @csrf
+                                    <input type="hidden" name="_method" value="delete" />
+                                    <button type="submit" class="btn btn-del hidden">
+                                        <i class="fa fa-fw fa-times-circle"></i> delete
+                                    </button>
+                                </form>
+                            </li>
 
-                            </ul>
-                        </div>
-                    </td>
-                </tr>
+                        </ul>
+                    </div>
+                </td>
+            </tr>
             @endforeach
-            </tbody>
-        </table>
-    </div>
+        </tbody>
+    </table>
+</div>
 @endsection
