cc6d87bc692dc51fd1bc5e585d69d9591feccb14
Taher Fakhruddin Makadam <mukadam.taher@gmail.com>
2019-12-19 21:12:36 +0300

    changes

--- resources/views/frontend/partials/_shipping_message.blade.php
+++ resources/views/frontend/partials/_shipping_message.blade.php
@@ -1,9 +1,9 @@
-<section class="page-section">
+{{--<section class="page-section">
     <div class="container">
         <div class="message-box">
             <div class="message-box-inner">
                 <h2>{{ trans('message.shipping_message') }}</h2>
-            </div>
-        </div>
-    </div>
-</section>
+</div>
+</div>
+</div>
+</section>--}}
